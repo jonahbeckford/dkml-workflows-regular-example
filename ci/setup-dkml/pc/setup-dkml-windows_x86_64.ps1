76409792e27fbb4a8a52524626b5e2ae7c3d2f5a
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-11-03 21:27:12 -0700

    upgrade dkml-workflows

--- ci/setup-dkml/pc/setup-dkml-windows_x86_64.ps1
+++ ci/setup-dkml/pc/setup-dkml-windows_x86_64.ps1
@@ -502,7 +502,7 @@ set -euf
 # Constants
 SHA512_DEVNULL='cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e'
 #   Edited by https://gitlab.com/diskuv/diskuv-ocaml/contributors/release.sh
-DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=89c5d7acd8bd21af078e30403830986f338a4a5b
+DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=cc9518fa5630bfbe24d4c5e0e2cc29af513037ce
 # Constants
 #   Should be edited by release.sh, but ...
 #   Can't be 1.0.0 or later until https://github.com/ocaml/opam-repository/pull/21704 ocaml-option-32bit
